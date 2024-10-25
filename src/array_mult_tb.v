a41ca82e0d692179439564c592b63209abec04b3
Nick Pham <ntp8058@ad.nyu.edu>
2024-10-25 08:39:35 -0400

    verilog files

--- src/array_mult_tb.v
+++ src/array_mult_tb.v
@@ -1,20 +1,4 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
-`default_nettype none
-
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
-);
+`timescale 1ns / 1ps
 
 module array_mult_tb;
 
@@ -126,7 +110,5 @@ always @(posedge check_timer) begin
 		failures = failures + 1;
 	end
 end
-
-endmodule
 
 endmodule
