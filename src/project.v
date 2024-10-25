561f45f63bcba75ec81effa0d5a31cb850f66cbe
Nick Pham <ntp8058@ad.nyu.edu>
2024-10-25 09:18:23 -0400

    comma

--- src/project.v
+++ src/project.v
@@ -43,7 +43,7 @@ module tt_um_multiplier (
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, uio_in 1'b0};
+  wire _unused = &{ena, clk, rst_n, uio_in, 1'b0};
 endmodule
 
 
